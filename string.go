7fdab0fab3a2180e8ca088a194ea254be03d2b18
Agus Pribadi <aguspribadi368@gmail.com>
2023-10-02 10:15:49 +0700

    first commit

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Agus")
+	fmt.Println("Agus Pribadi")
+	fmt.Println("Agus Pribadi Tiga Tiga Sembilan")
+
+	fmt.Println(len("Agus"))
+	fmt.Println("Agus Pribadi"[0])
+	fmt.Println("Agus Pribadi Tiga Tiga Sembilan"[1])
+}
---
+++ variable.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Agus Pribadi"
+	fmt.Println(name)
+
+	name = "Agus Pribadi Tiga Tiga Sembilan"
+	fmt.Println(name)
+
+	var friendName = "Ucup"
+	fmt.Println(friendName)
+
+	var age = 30
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Print(country)
+	
+	var (
+		firstName = "Agus"
+		lastName = "Pribadi"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
